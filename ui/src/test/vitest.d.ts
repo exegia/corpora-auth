eb7dd5baee139bfda5fc591863d27fe9e168efe3
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-07-31 17:22:58 -0700

    change directory name

--- ui/src/test/vitest.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AxeMatchers } from "vitest-axe/matchers";
-import "vitest";
-
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Assertion<T = unknown> extends AxeMatchers {}
-  interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
